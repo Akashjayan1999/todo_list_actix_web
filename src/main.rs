0f28725984bd56f191b94399a5db13b117e7ec55
Akashjayan1999 <akashjayan999@gmail.com>
2025-09-01 05:33:17 +0000

    complete todo crud

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use actix_web::{web::{self, Json}, HttpResponse, Responder};
 use sqlx::{self,SqlitePool,prelude::*};
-fn main() {
-    println!("Hello, world!");
+
+#[actix_web::main]
+async fn main() {
+    let pool = db().await;
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .route("/todo_list", web::get().to(get_todo_list))
+            .route("/add_todo", web::post().to(add_todo))
+            .route("/todo/{id}", web::get().to(get_single_todo))
+            .route("/delete_todo/{id}", web::delete().to(delete_todo))
+            .route("/update_todo/{id}", web::put().to(update_todo))
+            // .service(actix_files::Files::new("/static", "static").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
 }
 
 async fn db() -> SqlitePool {
@@ -46,6 +63,22 @@ async fn delete_todo(id:web::Path<i32>, pool:web::Data<SqlitePool>) -> impl Resp
     }else{
          sqlx::query("DELETE FROM todos WHERE id = ?1").bind(&id).execute(pool.get_ref()).await.unwrap();
          HttpResponse::Ok().body("Todo deleted")
+    }
+    
+}
+
+
+async fn update_todo(id:web::Path<i32>, pool:web::Data<SqlitePool>,todo:Json<TodoRequest>) -> impl Responder {
+    let id = id.into_inner();
+
+    let row :Vec<Todo> = sqlx::query_as("SELECT * FROM todos WHERE id = ?1").bind(&id).fetch_all(pool.get_ref()).await.unwrap();
+    if(row.len() == 0){
+        let msg = format!("Todo with id {} not found", id);
+        return HttpResponse::NotFound().body(msg);
+    }else{
+        sqlx::query("UPDATE todos SET content = ?1 WHERE id = ?2").bind(&todo.content).bind(&id).execute(pool.get_ref()).await.unwrap();
+     
+        HttpResponse::Ok().body("todo updated")
     }
     
 }
